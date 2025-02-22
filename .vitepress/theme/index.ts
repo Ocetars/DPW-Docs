faec3421f02891207ecd0c8e4e1c1e2b0d227dd5
Ocetars <ocetars@gmail.com>
2025-02-22 19:12:05 +0800

    Add Vercel Analytics

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,11 @@
 import Theme from 'vitepress/theme'
 import './style/var.css'
+import { inject } from '@vercel/analytics'
+
+// 初始化 Vercel Analytics
+if (typeof window !== 'undefined') {
+  inject()
+}
 
 export default {
   ...Theme
